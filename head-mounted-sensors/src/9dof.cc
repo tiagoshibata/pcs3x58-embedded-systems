c1c22b0862d3b874fc93f74643cc02c16dc21d79
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-11-01 16:55:42 -0300

    Rotate pitch axis 180 degrees

--- head-mounted-sensors/src/9dof.cc
+++ head-mounted-sensors/src/9dof.cc
@@ -35,7 +35,8 @@ bool Serialize9Dof::serialize(int8_t *buffer) {
     RTIMU_DATA imuData = imu->getIMUData();
     if (!imuData.fusionPoseValid)
         return false;
-    for (int i = 0; i < 3; i++)
+    buffer[0] = ((int)((imuData.fusionPose.data(0) + PI) * (127 / PI))) & 0xff;
+    for (int i = 1; i < 3; i++)
         buffer[i] = imuData.fusionPose.data(i) * (127 / PI);
     // printf("%f %f %f\n", imuData.fusionPose.x(), imuData.fusionPose.y(), imuData.fusionPose.z());
     return true;
