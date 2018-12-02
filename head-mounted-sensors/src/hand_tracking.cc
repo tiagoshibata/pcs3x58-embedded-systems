50bd86189851be7664b6d160586a39e7d0028ed3
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-12-02 17:42:10 -0200

    Replace morphological open/close with erode/dilate for performance

--- head-mounted-sensors/src/hand_tracking.cc
+++ head-mounted-sensors/src/hand_tracking.cc
@@ -7,7 +7,7 @@
 
 HandTracking::HandTracking(bool debug) {
     std::ifstream is("/etc/pimpolho");
-    int threshold_upper = 25, open_kernel_radius = 8, close_kernel_radius = 12;
+    int threshold_upper = 25, open_kernel_radius = 8, close_kernel_radius = 16;
     if (is) {
         is >> threshold_upper >> open_kernel_radius >> close_kernel_radius;
     }
@@ -20,36 +20,34 @@ HandTracking::HandTracking(bool debug) {
     depth_intrinsics = dev->get_stream_intrinsics(rs::stream::depth);
 
     cv::Size size(depth_intrinsics.width, depth_intrinsics.height);
-    cv::Mat open_kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(open_kernel_radius, open_kernel_radius));
-    cv::Mat close_kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(close_kernel_radius, close_kernel_radius));
-    auto depth_callback = [size, threshold_upper, open_kernel, close_kernel, debug](rs::frame f) {
+    cv::Mat erode_kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(open_kernel_radius, open_kernel_radius));
+    cv::Mat dilate_kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(close_kernel_radius, close_kernel_radius));
+    auto depth_callback = [size, threshold_upper, erode_kernel, dilate_kernel, debug](rs::frame f) {
         static cv::VideoWriter video_converted, video_threshold, video_open, video_close;
         cv::Mat frame16(size, CV_16UC1, (uint16_t*) f.get_data());
         cv::Mat frame;
         frame16.convertTo(frame, CV_8U, 1. / 32);
-        cv::Mat binary_mask(size, CV_8U);
-        // cv::threshold(frame, binary_mask, 30, 255, cv::THRESH_BINARY_INV);
-        cv::inRange(frame, 5, threshold_upper, binary_mask);
         if (debug) {
-            std::cout << "Frame\n";
             if (!video_converted.isOpened()) {
                 video_converted.open("converted.avi", cv::VideoWriter::fourcc('X', 'V', 'I', 'D'), 15, size, false);
                 video_threshold.open("threshold.avi", cv::VideoWriter::fourcc('X', 'V', 'I', 'D'), 15, size, false);
-            }
-            video_converted << frame;
-            video_threshold << binary_mask;
-        }
-        cv::erode(binary_mask, frame, open_kernel);
-        cv::dilate(frame, binary_mask, close_kernel);
-        // cv::morphologyEx(binary_mask, frame, cv::MORPH_OPEN, open_kernel);
-        // cv::morphologyEx(frame, binary_mask, cv::MORPH_CLOSE, close_kernel);
-        if (debug) {
-            if (!video_open.isOpened()) {
                 video_open.open("open.avi", cv::VideoWriter::fourcc('X', 'V', 'I', 'D'), 15, size, false);
                 video_close.open("close.avi", cv::VideoWriter::fourcc('X', 'V', 'I', 'D'), 15, size, false);
             }
+            video_converted << frame;
+        }
+        // cv::morphologyEx(binary_mask, frame, cv::MORPH_OPEN, open_kernel);
+        // cv::morphologyEx(frame, binary_mask, cv::MORPH_CLOSE, close_kernel);
+        cv::inRange(frame, 5, threshold_upper, frame);
+        if (debug)
+            video_threshold << frame;
+        cv::erode(frame, frame, erode_kernel);
+        if (debug)
             video_open << frame;
-            video_close << binary_mask;
+        cv::dilate(frame, frame, dilate_kernel);
+        if (debug)
+            video_close << frame;
+
         }
     };
     dev->set_frame_callback(rs::stream::depth, depth_callback);
